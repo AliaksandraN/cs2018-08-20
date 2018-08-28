263cecf15bf1d2bbc10da4d81327193c74a61fd7
AliaksandraN <Semolina11>
2018-08-28 21:46:40 +0300

    lesson04

--- src/by/it/anovikava/lesson04/TaskC2.java
+++ src/by/it/anovikava/lesson04/TaskC2.java
@@ -1,5 +1,8 @@
 package by.it.anovikava.lesson04;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +30,24 @@ package by.it.anovikava.lesson04;
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите день и месяц: ");
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        //LocalDate a = LocalDate.of(2018, month, day); это метод нахождения дня года
+        // System.out.println(a.getDayOfYear());
+        int[ ] dy = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
 
-}
+        int res=0;
+
+        for (int i = 0; i < month-1; i++) {
+            res += dy[i];
+
+        }
+        System.out.println(res+day);
+
+    }
+    }
+
+
