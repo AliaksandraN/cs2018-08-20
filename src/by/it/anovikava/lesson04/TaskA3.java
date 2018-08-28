9e44159c1fade12b020277126950415aa863d66a
AliaksandraN <Semolina11>
2018-08-28 13:49:54 +0300

    lesson04

--- src/by/it/anovikava/lesson04/TaskA3.java
+++ src/by/it/anovikava/lesson04/TaskA3.java
@@ -22,6 +22,21 @@ package by.it.anovikava.lesson04;
  */
 
 
+import java.util.Scanner;
+
 public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите три любых целых числа: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        if (n1==n2 && n2 == n3)
+            System.out.println(n1 + " " + n2 + " " + n3);
+        else if (n1 == n2) System.out.println(n1 + " " + n2);
+        else if (n1 == n3) System.out.println(n1 + " " + n3);
+        else if (n2 == n3) System.out.println(n2 + " " + n3);
+
+                   }
 
 }
