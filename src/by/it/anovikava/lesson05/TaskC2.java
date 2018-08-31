6fb4a732698fa98f5c229ff53781a74e8f74f156
AliaksandraN <Semolina11>
2018-08-31 08:19:32 +0300

    lesson05

--- src/by/it/anovikava/lesson05/TaskC2.java
+++ src/by/it/anovikava/lesson05/TaskC2.java
@@ -4,15 +4,38 @@ package by.it.anovikava.lesson05;
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
 
+import java.util.Scanner;
+
 public class TaskC2 {
     //Напишите тут ваш код
-
-
-
-
-
+    int a,b,t;
     public static void sort(int[] array) {
         //Напишите тут ваш код
+        int t;
+        for(int a=1; a < array.length; a++)
+            for(int b=array.length-1; b >= a; b--) {
+                if(array[b-1] < array[b]) { // if out of order
+                    // exchange elements
+                    t = array[b-1];
+                    array[b-1] = array[b];
+                    array[b] = t;
+                }
+            }
+        for(int x : array)
+            System.out.print(x+"\n");
+
+
     }
+    public static void main(String[] args) {
+        int[]arr = new int[20];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 20 чисел: ");
+        for (int i = 0; i <20; i++) {
+            int value = sc.nextInt();
+            arr[i] = value;
+        }
+        sort(arr);
+    }
+
 
 }
